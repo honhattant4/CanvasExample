054bd53fb6e3c45d00ee1cc7ef26a6698259bf20
TanHN <tanhn@asiantech.vn>
2016-08-01 14:16:21 +0700

    first

---
+++ app/src/main/java/com/neolab/canvasexample/CanvasView.java
@@ -0,0 +1,23 @@
+package com.neolab.canvasexample;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Copyright © 2016 AsianTech Inc.
+ * Created by TanHN on 8/1/16.
+ */
+public class CanvasView extends View {
+    public CanvasView(Context context) {
+        super(context);
+    }
+
+    public CanvasView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CanvasView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
